06fa136298a7ceafaf490ea7480a79ecfa1f87e1
gabberr <gaber.setina@gmail.com>
2014-11-14 16:18:57 +0100

    Added printWeekOrderByParts to week class.

--- SCM_Example_1/src/at/fhooe/mcm/scm/model/Week.java
+++ SCM_Example_1/src/at/fhooe/mcm/scm/model/Week.java
@@ -1,5 +1,6 @@
 package at.fhooe.mcm.scm.model;
 
+import java.io.Console;
 import java.util.ArrayList;
 
 public class Week {
@@ -17,7 +18,10 @@ public class Week {
 	int nOfD;
 	int nOfE;
 	
-	public Week(int nOfA, int nOfB, int nOfC, int nOfD, int nOfE) {
+	String weekName = "";
+	
+	public Week(String name, int nOfA, int nOfB, int nOfC, int nOfD, int nOfE) {
+		weekName = name;
 		products.add(proA);
 		products.add(proB);
 		products.add(proC);
@@ -72,6 +76,15 @@ public class Week {
 			sum += products.get(i).getP5();
 		}
 		return sum;
+	}
+	public void printWeekOrderByParts() {
+		System.out.println(weekName + " P1: " +getTotalP1() +
+				" P2: "+ getTotalP2()+
+				" P3: "+ getTotalP3()+
+				" P4: "+ getTotalP4()+
+				" P5: "+ getTotalP5());
+		
+		
 	}
 
 }
