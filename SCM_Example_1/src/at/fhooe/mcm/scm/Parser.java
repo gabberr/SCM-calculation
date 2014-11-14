cc4595f9d4f205bf459478585e964734a8537e26
Phil <philipp.koller33@gmail.com>
2014-11-14 16:03:32 +0100

    Add model

--- SCM_Example_1/src/at/fhooe/mcm/scm/Parser.java
+++ SCM_Example_1/src/at/fhooe/mcm/scm/Parser.java
@@ -1,6 +1,7 @@
 package at.fhooe.mcm.scm;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,10 +10,15 @@ public class Parser {
 	 
 	  public void parseObjects() {
 	 
-		String csvFile = "/Users/mkyong/Downloads/GeoIPCountryWhois.csv";
+		ClassLoader classLoader = getClass().getClassLoader();
+	//	File file2 = new File(this.getClass().getResource("SCM_Programming_Example_1.csv"));
+	//	File file = new File(classLoader.getResource("SCM_Programming_Example_1.csv").getFile());
+//		String file = p.getFileName().toString();
+		String csvFile = "C://Users//Philipp//Desktop//Hagenberg//SupplyChain//SCM-calculation//SCM_Example_1//resources//SCM_Programming_Example_1.csv";
+//		String csvFileGaber = "C://Users//Philipp//Desktop//Hagenberg//SupplyChain//SCM-calculation//SCM_Example_1//resources//SCM_Programming_Example_1.csv";
 		BufferedReader br = null;
 		String line = "";
-		String cvsSplitBy = ",";
+		String cvsSplitBy = ";";
 	 
 		try {
 	 
@@ -20,11 +26,7 @@ public class Parser {
 			while ((line = br.readLine()) != null) {
 	 
 			        // use comma as separator
-				String[] country = line.split(cvsSplitBy);
-	 
-				System.out.println("Country [code= " + country[4] 
-	                                 + " , name=" + country[5] + "]");
-	 
+			//  country = line.split(cvsSplitBy);
 			}
 	 
 		} catch (FileNotFoundException e) {
@@ -42,7 +44,6 @@ public class Parser {
 		}
 	 
 		System.out.println("Done");
-	  }
 	 
 	}
 }
