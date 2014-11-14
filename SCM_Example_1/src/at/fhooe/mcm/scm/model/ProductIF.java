563ab8dcc766eeb58c7f288796e0b9948a4b7cc9
gabberr <gaber.setina@gmail.com>
2014-11-14 15:48:44 +0100

    Added interfae ProductIF

---
+++ SCM_Example_1/src/at/fhooe/mcm/scm/model/ProductIF.java
@@ -0,0 +1,11 @@
+package at.fhooe.mcm.scm.model;
+
+public interface ProductIF {
+	
+	public int getP1();
+	public int getP2();
+	public int getP3();
+	public int getP4();
+	public int getP5();
+
+}
---
+++ SCM_Example_1/src/at/fhooe/mcm/scm/model/ProduktA.java
@@ -0,0 +1,35 @@
+package at.fhooe.mcm.scm.model;
+
+public class ProduktA implements ProductIF {
+
+	@Override
+	public int getP1() {
+		
+		return 1;
+	}
+
+	@Override
+	public int getP2() {
+		
+		return 0;
+	}
+
+	@Override
+	public int getP3() {
+		
+		return 1;
+	}
+
+	@Override
+	public int getP4() {
+		
+		return 2;
+	}
+
+	@Override
+	public int getP5() {
+		
+		return 0;
+	}
+
+}
