112d582295f47d64e908873907c69a01a870a261
gabberr <gaber.setina@gmail.com>
2014-11-14 17:25:11 +0100

    no message

--- SCM_Example_1/src/at/fhooe/mcm/scm/model/Stock.java
+++ SCM_Example_1/src/at/fhooe/mcm/scm/model/Stock.java
@@ -1,5 +1,7 @@
 package at.fhooe.mcm.scm.model;
 
+import java.util.ArrayList;
+
 public class Stock {
 
 	public int nOfP1 = 216;
@@ -15,6 +17,7 @@ public class Stock {
 	public static int reorderTimePart5 = 1;
 	
 	public int iteration;
+	ArrayList<Order> placedOrders = new ArrayList<Order>();
 	
 	public Stock(){
 		iteration = 0;
@@ -25,6 +28,51 @@ public class Stock {
 		
 		iteration ++;
 	}
-	
+	public void restock(){
+		for 
+	}
+	public void placeOrder(int partId, int ammount) {
+		
+		switch (partId) {
+		case 1:
+			Order o1 = new Order(iteration + reorderTimePart1, partId, ammount);
+			placedOrders.add(o1);
+			break;
+		case 2:
+			Order o2 = new Order(iteration + reorderTimePart2, partId, ammount);
+			placedOrders.add(o2);
+			break;
+		case 3:
+			Order o3 = new Order(iteration + reorderTimePart3, partId, ammount);
+			placedOrders.add(o3);
+			break;
+		case 4:
+			Order o4 = new Order(iteration + reorderTimePart4, partId, ammount);
+			placedOrders.add(o4);
+			break;
+		case 5:
+			Order o5 = new Order(iteration + reorderTimePart5, partId, ammount);
+			placedOrders.add(o5);
+			break;
+
+		default:
+			break;
+		}
+
+	}
+	private class Order {
+		int weekOfDelivery;
+		int partId;
+		int ammount;
+		public Order(int weekOfDelivery, int partId, int ammount) {
+			super();
+			this.weekOfDelivery = weekOfDelivery;
+			this.partId = partId;
+			this.ammount = ammount;
+		}
+		public boolean isCurrentWeek(int week) {
+			return week == weekOfDelivery;
+		}
+	}
 	
 }
